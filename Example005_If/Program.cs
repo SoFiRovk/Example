120a755df13c1d50ed8a97b000d8e37003cdef1d
SonyaR <Sonya@mail.ru>
2022-10-18 21:30:43 +0800

    .\Example005_If\

---
+++ Example005_If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "софи")
+{
+    Console.WriteLine("Ура,это же Софи");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
